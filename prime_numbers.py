0b30bc9075165e3e81e821d836756ced82a61da2
danielwoldanski <46328977+danielwoldanski@users.noreply.github.com>
2019-03-16 11:17:43 +0100

    Create prime_numbers.py

---
+++ prime_numbers.py
@@ -0,0 +1,22 @@
+print("Program sprawdzi czy istnieją liczby pierwsze mniejsze od podanej przez użytkownika")
+
+x = input("Podaj liczbę: ")
+y = int(x)
+
+lista=[]
+i = y - 1
+
+for liczba in range (2,y+1):
+    lista.append(liczba)
+
+while y>1:
+    while i>1:
+        wynik = y/i
+        if lista.__contains__(wynik):
+            lista.remove(y)
+            break
+        i -= 1
+    y -= 1
+    i = y - 1
+print("Oto lista liczb pierwszych mniejszych od podanej:")
+print(lista)
